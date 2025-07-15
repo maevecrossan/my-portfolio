a15a09a7654b2326c2286a3b9f8fe00b9a76de67
Maeve Crossan <maevecrossan@gmail.com>
2025-07-15 14:22:22 +0100

    Update layout to use tab spacing.

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,15 +2,15 @@
 import Link from 'next/link';
 
 export default function Navbar() {
-  return (
-    <nav>
-      <h1>Maeve Crossan</h1>
-      <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/previous-work">Previous Work</Link></li>
-        <li><Link href="/about">About Me</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav>
+			<h1>Maeve Crossan</h1>
+			<ul>
+				<li><Link href="/">Home</Link></li>
+				<li><Link href="/previous-work">Previous Work</Link></li>
+				<li><Link href="/about">About Me</Link></li>
+				<li><Link href="/contact">Contact</Link></li>
+			</ul>
+		</nav>
+	);
 }

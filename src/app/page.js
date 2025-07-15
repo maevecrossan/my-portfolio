32dde3af2721a607693982565e966652b46ac4e8
Maeve Crossan <maevecrossan@gmail.com>
2025-07-15 14:19:53 +0100

    Update home page title.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Homepage() {
-    return <h1> Maeve Crossan </h1>
+    return <h1> Home page </h1>
 }
 
 // export default function Home() {

4c659fea2933aa46d8be42795baabe6c350cc20d
Maeve Crossan <maevecrossan@gmail.com>
2025-07-15 15:03:39 +0100

    Add basic styling using tailwind. Add commented out burger nav.

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,14 +3,23 @@ import Link from 'next/link';
 
 export default function Navbar() {
 	return (
-		<nav>
-			<h1>Maeve Crossan</h1>
-			<ul>
-				<li><Link href="/">Home</Link></li>
-				<li><Link href="/previous-work">Previous Work</Link></li>
-				<li><Link href="/about">About Me</Link></li>
-				<li><Link href="/contact">Contact</Link></li>
+		<nav className="bg-gray-100">
+			<h1 className="flex text-center"> Maeve Crossan </h1>
+			{/* Desktop Nav */}
+			<ul className="flex gap-4 list-none p-4">
+				<li><Link href="/" className="hover:underline">Home</Link></li>
+				<li><Link href="/previous-work" className="hover:underline">Previous Work</Link></li>
+				<li><Link href="/about" className="hover:underline">About Me</Link></li>
+				<li><Link href="/contact" className="hover:underline">Contact</Link></li>
 			</ul>
+
+			{/* Burger Nav */}
+			{/* <ul className="flexbox list-none p-4">
+				<li><Link href="/" className="hover:underline">Home</Link></li>
+				<li><Link href="/previous-work" className="hover:underline">Previous Work</Link></li>
+				<li><Link href="/about" className="hover:underline">About Me</Link></li>
+				<li><Link href="/contact" className="hover:underline">Contact</Link></li>
+			</ul> */}
 		</nav>
 	);
 }

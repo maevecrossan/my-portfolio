772d8604c9295dcfdc1a95ac7c72c7cc137a74e1
Maeve Crossan <maevecrossan@gmail.com>
2025-07-16 14:43:37 +0100

    Add min height to body.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,20 @@
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
-import './globals.css';
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import "./globals.css";
 
 export const metadata = {
-	title: 'My Portfolio Site',
-	description: 'Showcasing my work and profile',
+    title: "My Portfolio Site",
+    description: "Showcasing my work and profile",
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-		<body>
-			<Navbar />
-			<main>{children}</main>
-			<Footer />
-		</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body className="min-h-screen flex flex-col">
+                <Navbar />
+                <main className="flex-grow">{children}</main>
+                <Footer />
+            </body>
+        </html>
+    );
 }

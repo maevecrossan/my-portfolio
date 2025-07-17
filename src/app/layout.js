b71cce2e52c85b21c30f343b0e96752834ebc24d
Maeve Crossan <maevecrossan@gmail.com>
2025-07-17 16:14:57 +0100

    Import fonts and add template literals to html tag.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,16 @@
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import "./globals.css";
+import { dmserif, roboto, teko } from "./fonts";
 
 export const metadata = {
     title: "My Portfolio Site",
     description: "Showcasing my work and profile",
 };
 
-
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" className={`${dmserif.variable} ${roboto.variable} ${teko.variable}`}>
             <body className="min-h-screen flex flex-col">
                 <Navbar />
                 <main className="flex-grow">{children}</main>

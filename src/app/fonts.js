402f6ffe7c5d31d2209d64d82311d5f1788b6233
Maeve Crossan <maevecrossan@gmail.com>
2025-07-17 16:12:23 +0100

    Format

---
+++ src/app/fonts.js
@@ -0,0 +1,25 @@
+import localFont from "next/font/local";
+
+export const dmserif = localFont({
+    src: "../fonts/DMSerifText-Regular.ttf",
+    variable: "--font-dmserif",
+    weight: "400",
+    style: "normal",
+    display: "swap",
+});
+
+export const roboto = localFont({
+    src: "../fonts/Roboto-VariableFont_wdth,wght.ttf",
+    variable: "--font-roboto",
+    display: "swap",
+});
+
+export const teko = localFont({
+    src: [
+        { path: "../fonts/Teko-Light.ttf", weight: "400", style: "normal" },
+        { path: "../fonts/Teko-Medium.ttf", weight: "500", style: "normal" },
+        { path: "../fonts/Teko-Bold.ttf", weight: "700", style: "normal" },
+    ],
+    variable: "--font-teko",
+    display: "swap",
+});

134f3f1a941425645837c5cf0be6acd86d8ff8a7
Maeve Crossan <maevecrossan@gmail.com>
2025-07-17 16:15:25 +0100

    Remove defunct background colour class.

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Homepage() {
     return (
-        <main className="bg-zinc-800 flex flex-col items-center justify-center min-h-screen">
+        <main className="flex flex-col items-center justify-center min-h-screen font-roboto">
             <h2>Home</h2>
         </main>
     );

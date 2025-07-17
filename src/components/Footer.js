a633707e6adf3b40f5adf03a31e84ab7b606485e
Maeve Crossan <maevecrossan@gmail.com>
2025-07-17 17:35:36 +0100

    Change bg colour of footer to match navbar.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer className="bg-ny-pink mt-8">
-            <div className="flex flex-col items-center shadow-inner px-4 py-6 space-y-4">
+            <div className="flex flex-col items-center shadow-inner px-4 py-6 bg-zinc-900 space-y-4">
                 {/* Stacked Nav Links */}
                 <div className="flex flex-col items-center space-y-2">
                     <Link href="/" className="btn btn-ghost btn-sm">Homepage</Link>

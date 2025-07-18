2de0fbf045815cea6c093409a3b8d2b9538b6337
Maeve Crossan <maevecrossan@gmail.com>
2025-07-18 15:54:14 +0100

    Update subheading level for improved accessibility and semantic structure.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,7 @@
 export default function AboutMePage() {
     return (
         <main className="flex flex-col items-center justify-center min-h-screen">
-            <h2>About Me</h2>
+            <h3>About Me</h3>
         </main>
     )
 }
--- src/app/previous-work/page.js
+++ src/app/previous-work/page.js
@@ -1,7 +1,7 @@
 export default function PreviousWorkPage() {
     return (
         <main className="flex flex-col items-center justify-center min-h-screen">
-            <h2>Previous Work</h2>
+            <h3>Previous Work</h3>
         </main>
     )
 }

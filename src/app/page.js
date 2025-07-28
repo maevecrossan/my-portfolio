03783e7f1fd4c9de8c021c01e9a70448ee0fd199
Maeve Crossan <maevecrossan@gmail.com>
2025-07-28 18:31:21 +0100

    Update button colours.

--- src/app/page.js
+++ src/app/page.js
@@ -37,8 +37,8 @@ const images = [
 export default function Homepage() {
     return (
         <main className="flex flex-col items-center justify-center min-h-screen p-4">
-            <div className="rounded-box m-10 p-10 bg-white dark-grey-font shadow-xl">
-                <h3 className="mb-6 text-2xl font-semibold font-dmserif underline">
+            <div className="rounded-box m-10 p-10 bg-white/50 text-zinc-800 shadow-xl">
+                <h3 className="mb-6 text-2xl font-semibold font-dmserif ">
                     Hi there!
                 </h3>
                 <p>I&apos;m Maeve, nice to meet you.</p>
@@ -47,29 +47,29 @@ export default function Homepage() {
                     end development. You can learn more about me by clicking the
                     button below.
                 </p>
-                <button className="btn btn-neutral rounded mt-5">
+                <button className="btn btn-soft btn-secondary rounded mt-5">
                     <Link href="/about">Learn more</Link>
                 </button>
             </div>
 
-            <div className="rounded-box w-full max-w-screen-lg mx-auto px-4 py-10 bg-zinc-900 shadow-xl flex flex-col items-center">
-                <h4 className="mb-6 text-2xl font-semibold font-dmserif text-center underline">
+            <div className="rounded-box w-full max-w-screen-lg mx-auto px-4 py-10 bg-white/50 shadow-xl text-zinc-800 flex flex-col items-center">
+                <h4 className="mb-6 text-2xl font-semibold font-dmserif text-center ">
                     Previous Work
                 </h4>
                 <Carousel images={images} />
-                <button className="btn rounded mt-5">
+                <button className="btn btn-soft btn-secondary rounded mt-5">
                     <Link href="/previous-work">View Previous Work</Link>
                 </button>
             </div>
 
-            <div className="rounded-box m-10 p-10 bg-white dark-grey-font shadow-xl">
-                <h4 className="mb-6 text-2xl font-semibold font-dmserif underline">
+            <div className="rounded-box m-10 p-10 bg-white/50 text-zinc-800 shadow-xl">
+                <h4 className="mb-6 text-2xl font-semibold font-dmserif  ">
                     Have a question?
                 </h4>
                 <p>
                     Do you have a question about me or my work? Have a project in mind? Reach out to me below and lets connect.
                 </p>
-                <button className="btn btn-neutral rounded mt-5">
+                <button className="btn btn-soft btn-secondary rounded mt-5">
                     <Link href="/contact">Get in touch</Link>
                 </button>
             </div>
